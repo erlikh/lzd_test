ed599163c6931142bfad6ae9bda644e3e5be4097
Nick Erlikh <n.erlikh@gmail.com>
2014-01-16 23:56:07 +0700

    add deferred calls

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var express = require('express')
   , http = require('http')
   , request = require('request')
   , cheerio = require('cheerio')
+  , q = require('q')
   , path = require('path');
 
 var app = express();
@@ -32,21 +33,46 @@ app.get('/', routes.index);
 
 app.post('/compare', function(req, res){
   var first_url = req.body.urls[0],
-    second_url = req.body.urls[1]
+    second_url = req.body.urls[1],
+    specs = {}
   ;
 
-  request({uri: first_url}, function(err, response, body){
-    if(err && response.statusCode !== 200){console.log( 'Request error.' );}
+  //TODO(NE): check if URLs present.
 
+  var parse_specs = function(body){
     var $ = cheerio.load(body)
-      , specs = $('#productSpecifications');
+      , specs_containers = $('#productSpecifications tr')
+      , deferred = q.defer();
     ;
 
-    console.log("specs: ", specs.html());
-  });
+    specs_containers.each(function(){
+      var title = $(this).find('th').html(); //NOTE(NE): SKU has different structure but I don't think we need it.
+      if(!title){ return true; }
+      var value = $(this).find('td').html();
 
-  console.log("req.body: ", req.body);
-  res.render('index', { title: 'Express' });
+      console.log(title + ': ' + value);
+    });
+
+    deferred.resolve();
+  };
+
+  var make_request = function(url){
+    console.log("\nurl: ", url)
+    var deferred = q.defer();
+    request({uri: url}, function(err, response, body){
+      if(err && response.statusCode !== 200){console.log( 'Request error.' );} //TODO(NE): stop the queue.
+      deferred.resolve(body);
+    });
+
+    return deferred.promise;
+  };
+
+
+  make_request(first_url)
+    .then(parse_specs)
+    .then(function(){ return make_request(second_url); })
+    .then(parse_specs)
+  ;
 });
 
 http.createServer(app).listen(app.get('port'), function(){
