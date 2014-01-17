203d40679e96aa582f5c3ca4266da5e4061208ab
Nick Erlikh <n.erlikh@gmail.com>
2014-01-17 08:14:13 +0700

    respond with compare table

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,16 +1,16 @@
 $(function(){
 
-  $('form').submit(function(e){
+  $('[data-role="urls_form"]').submit(function(_){
     $.ajax({
       url: "/compare",
       type: "POST",
       data: $(this).serialize(),
-      success: function(){ console.log("arguments: ", arguments) },
+      success: function(data){
+        $('[data-role="compare"]').html(data);
+      },
       error: function(){}
     });
     return false;
   });
 
-
-
-})
+});
