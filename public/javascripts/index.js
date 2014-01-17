dc6c52d10d5c0ae8ea24a45bb42666d66bec237b
Nick Erlikh <n.erlikh@gmail.com>
2014-01-17 10:45:56 +0700

    add url validation

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,7 +3,7 @@ var lzd = {};
 lzd.compare = function(container){
   this.container = $(container);
   this.form = this.container.find('form');
-  this.results = this.container.find('[data-role="results"]')
+  this.results = this.container.find('[data-role="results"]');
   this.submit_button = this.container.find('[data-role="submit_button"]');
   this.init_events();
 };
@@ -12,37 +12,36 @@ lzd.compare.prototype.init_events = function(){
   this.form.validate({
     rules: {
       first_url: {
-        minlength: 2,
-        required: true
+        required: true,
+        url: true
       },
       second_url: {
-        minlength: 2,
-        required: true
+        required: true,
+        url: true
       }
     }
   });
   this.form.submit($.proxy(this.submit, this));
 };
 
-lzd.compare.prototype.submit = function(_){
-  if(this.form.valid()){
-    var that = this;
+lzd.compare.prototype.submit = function(e){
+  e.preventDefault();
+  if(!this.form.valid()){ return; }
 
-    this.submit_button.button('loading');
-    this.results.html('');
+  var that = this;
+  this.submit_button.button('loading');
+  this.results.html('');
 
-    $.ajax({
-      url: "/compare",
-      type: "POST",
-      data: that.form.serialize(),
-      success: function(data){
-        that.submit_button.button('reset');
-        that.results.html(data);
-      },
-      error: function(){ alert('error') }
-    });
-  }
-  return false;
+  $.ajax({
+    url: "/compare",
+    type: "POST",
+    data: that.form.serialize(),
+    success: function(data){
+      that.submit_button.button('reset');
+      that.results.html(data);
+    },
+    error: function(){ alert('error') }
+  });
 };
 
 $(function(){
